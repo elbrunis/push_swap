522165b2362efee0de63691789794c7f80d492fa
Bruno iniesta <brunoiniestabarrera@gmail.com>
2025-05-03 12:37:17 +0200

    esta practicamente todo el manejo de entrada corregido

--- parsing.c
+++ parsing.c
@@ -5,23 +5,29 @@ static int is_digit(char *str)
 {
 	int	i;
 	int	sign;
-
+ 
 	i = 0;
 	sign = 0;
 	while (str[i])
 	{
-		while (str[i] == ' ' || str[i] == '\t')
-			i++;
 		if ((str[i] == '-' || str[i] == '+') && sign == 0)
 		{
 			sign = 1;
 			i++;
 		}
 		if (!str[i])
-			return (0);
+			return (1);
 		if (!ft_isdigit(str[i++]))
 			return (1);
 	}
+	return (0);
+}
+
+static int	check_int(char *str)
+{
+	if (ft_atol(str) != ft_atoi(str)) //vrango int min y el int max
+		return (1);
+	// get_max_min //funcion futura para guardar el numero maximo y el minimo
 	return (0);
 }
 
@@ -32,12 +38,17 @@ static int	check_array(char **array)
 	if (!array)
 		ft_error("no entro", NULL, NULL);
 	i = 0;
-	while (array[i])
+	while (array[i]) // verificacion de el int min
 	{
-		if (ft_atol(array[i]) != ft_atoi(array[i]))
+		if(is_digit(array[i]))
 		{
-			ft_error("INT_MIN", NULL, NULL);
 			free_array(array);
+			ft_error("invalid input", NULL, NULL);
+		}
+		if (check_int(array[i]))
+		{
+			free_array(array);
+			ft_error("INT_MIN", NULL, NULL);
 		}
 		i++;
 	}
@@ -56,14 +67,24 @@ int	check_argv(int argc, char **argv)
 	i = 1;
 	while(i < argc)
 	{
-		str = ft_strdup(argv[i++]);
+		str = ft_strdup(argv[i++]);// extraemos el argumento
 		if (!str)
 			ft_error("memory null", NULL, NULL);
-		if(is_digit(str))
-			ft_error("invalid input", NULL, NULL);
-		str_array = ft_split(str,' ');
-		if(!str_array[1])
+		str_array = ft_split(str, ' ');
+		if(!str_array[1]) // verificamos si es una string
+		{
 			free_array(str_array);
+			if(is_digit(str))
+			{
+				free(str);
+				ft_error("invalid input", NULL, NULL);
+			}
+			if (check_int(str))
+			{
+				free(str);
+				ft_error("INT_MIN", NULL, NULL);
+			}
+		}
 		else
 			check_array(str_array);
 		free(str);
